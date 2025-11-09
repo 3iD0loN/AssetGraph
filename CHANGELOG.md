ebff2472295660280b1ad55119726799ad93ad3e
Joel Barba <joel.barba@unity3d.com>
2025-11-08 20:56:39 -0600

    Updated sample project and package code to be compatible with 2022.3 LTS

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ All notable changes to this package will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [1.7.4] - 2025-11-08
+### Fixed
+- Upgraded sample Unity project to use 2022.3 LTS. Compile-time errors around AudioImporter have been fixed.
+
 ## [1.7.3] - 2025-11-08
 ### Fixed
 - Upgraded sample Unity project to use 2019.4 LTS. 
--- Editor/System/Importers/AudioImporterConfigurator.cs
+++ Editor/System/Importers/AudioImporterConfigurator.cs
@@ -41,7 +41,9 @@ namespace UnityEngine.AssetGraph
         {
             target.defaultSampleSettings = reference.defaultSampleSettings;
             target.forceToMono = reference.forceToMono;
-            target.preloadAudioData = reference.preloadAudioData;
+            var sampleSettings = target.defaultSampleSettings;
+            sampleSettings.preloadAudioData = reference.defaultSampleSettings.preloadAudioData;
+            target.defaultSampleSettings = sampleSettings;
 
             foreach (var g in NodeGUIUtility.SupportedBuildTargetGroups) {
                 var platformName = BuildTargetUtility.TargetToAssetBundlePlatformName (g,
@@ -93,7 +95,7 @@ namespace UnityEngine.AssetGraph
                 return false;
             if (target.ambisonic != reference.ambisonic)
                 return false;
-            if (target.preloadAudioData != reference.preloadAudioData)
+            if (target.defaultSampleSettings.preloadAudioData != reference.defaultSampleSettings.preloadAudioData)
                 return false;
 
             return true;
